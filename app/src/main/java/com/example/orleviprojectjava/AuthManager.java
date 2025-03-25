3880039cd3ad5e1e2aec196f098e812821dd85c0
or135 <orlevi490@gmail.com>
2025-03-25 19:53:37 +0200

    last

--- app/src/main/java/com/example/orleviprojectjava/AuthManager.java
+++ app/src/main/java/com/example/orleviprojectjava/AuthManager.java
@@ -5,7 +5,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
-
 import java.util.HashMap;
 import java.util.Map;
 
